414fc61e9e2aa29c1063ca3c70ef9feb476c9b1d
Djamilson <djamilson@gmail.com>
2020-04-21 15:28:27 -0300

    Init

---
+++ src/_config/host.js
@@ -0,0 +1,5 @@
+export default {
+  PORT: 3001,
+  // WEBHOST: '192.168.0.125',
+  WEBHOST: 'www.ofertadodia.palmas.br',
+};
---
+++ src/pages/Order/Table/TitleItem/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function RowItem() {
+  return (
+    <Container>
+      <tr>
+        <th>ID</th>
+        <th>Produto</th>
+        <th>Destinatário</th>
+        <th>Entregador</th>
+        <th>Cidade</th>
+        <th>Estado</th>
+        <th>Status</th>
+        <th colSpan={2}>Ação</th>
+      </tr>
+    </Container>
+  );
+}
